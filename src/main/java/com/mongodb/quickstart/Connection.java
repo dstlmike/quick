52a2a67cc2acd839326df92d366a72571fba5de2
dstlmike <17973597+dstlmike@users.noreply.github.com>
2023-11-04 03:53:32 -0400

    Update Connection.java

--- src/main/java/com/mongodb/quickstart/Connection.java
+++ src/main/java/com/mongodb/quickstart/Connection.java
@@ -6,11 +6,13 @@ import org.bson.Document;
 
 import java.util.ArrayList;
 import java.util.List;
-
+String DB_URI = process.env.URI;
+String DB_Collection = process.env.DB_Collection;
+String DB_DataBase = process.env.SampleDB;
 public class Connection {
 
     public static void main(String[] args) {
-        String connectionString = System.getProperty("mongodb+srv://gmgm:113hopest@cluster89278.oitd2c5.mongodb.net/sampledb?retryWrites=true&w=majority");
+        String connectionString = System.getProperty(uri); //"mongodb+srv://gmgm:113hopest@cluster89278.oitd2c5.mongodb.net/sampledb?retryWrites=true&w=majority");
         try (MongoClient mongoClient = MongoClients.create(connectionString)) {
             List<Document> databases = mongoClient.listDatabases().into(new ArrayList<>());
             databases.forEach(db -> System.out.println(db.toJson()));
